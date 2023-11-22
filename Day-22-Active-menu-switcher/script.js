978d845d771c7a2b0960c486d569761548222bb1
JadenDeanBotha <138444240+JadenDeanBotha@users.noreply.github.com>
2023-11-22 12:11:26 +0200

    Uploaded full app

---
+++ Day-22-Active-menu-switcher/script.js
@@ -0,0 +1,26 @@
+const links = document.querySelectorAll(".nav-list li a");
+
+for (link of links) {
+  link.addEventListener("click", smoothScroll);
+}
+
+function smoothScroll(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  document.querySelector(href).scrollIntoView({
+    behavior: "smooth",
+  });
+}
+
+// Active menu switcher
+
+const navList = document.querySelector(".nav-list");
+
+navList.addEventListener("click", (e) => {
+  const navLink = e.target.parentElement;
+
+  if (navLink.classList.contains("link")) {
+    navList.querySelector(".active").classList.remove("active");
+    navLink.classList.add("active");
+  }
+})
